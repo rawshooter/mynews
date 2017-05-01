7d0dd8985d2b58baecc67f2968353bac29120e9e
Thomas Alexnat <thomas@alexnat.de>
2017-05-01 13:59:25 +0200

    base mocking ui for experimental testing

---
+++ mynews/mynews/RSSController.swift
@@ -0,0 +1,39 @@
+//
+//  RSSController.swift
+//  mynews
+//
+//  Created by Thomas Alexnat on 01.05.17.
+//  Copyright © 2017 Thomas Alexnat. All rights reserved.
+//
+
+import UIKit
+
+class RSSController: UIViewController {
+
+    @IBAction func ActionRSSButton(_ sender: UIButton) {
+        print("load XML Data")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
